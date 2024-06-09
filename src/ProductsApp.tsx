6f1577de8393996c3361c67c32b43e3130c8a61d
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-06-09 17:10:56 -0600

    UI Kitten - Componentes estilizados

--- src/ProductsApp.tsx
+++ src/ProductsApp.tsx
@@ -1,13 +1,30 @@
 import 'react-native-gesture-handler';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './presentation/navigation/StackNavigation';
+import { useColorScheme } from 'react-native';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 export const ProductsApp = () => {
+
+    const colorScheme = useColorScheme();
+    const theme = colorScheme === 'dark' ? eva.dark : eva.light;
+
     return (
-        <NavigationContainer>
-            <StackNavigator />
-        </NavigationContainer>
+        <>
+            <IconRegistry icons={EvaIconsPack} />
+            <ApplicationProvider
+                {...eva} theme={ theme }
+            >
+                <NavigationContainer>
+                    <StackNavigator />
+                </NavigationContainer>
+            </ApplicationProvider>
+        </>
+        
+        
         
     );
 };
