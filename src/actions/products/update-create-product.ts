c38a90c10b83c8fd80d4a23831c4723b052a3186
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-06-13 10:34:59 -0600

    useMutation - TanStack

--- src/actions/products/update-create-product.ts
+++ src/actions/products/update-create-product.ts
@@ -4,8 +4,8 @@ import { Product } from "../../domain/entities/product";
 
 export const updateCreateProduct = (product: Partial<Product>) => {
 
-    product.stock = Number(product.stock);
-    product.price = Number(product.price);
+    product.stock = isNaN(Number(product.stock)) ? 0 : Number(product.stock);
+    product.price = isNaN(Number(product.price)) ? 0 : Number(product.price);
 
     if(product.id){
         return updateProduct(product);
@@ -17,13 +17,11 @@ export const updateCreateProduct = (product: Partial<Product>) => {
 
 //TODO: revisar si viene el usuario
 const updateProduct = async (product: Partial<Product>) => {
-    console.log({product});
 
     const { id, images = [], ...rest } = product;
 
     try {
         const checkedImages = prepareImages(images);
-        console.log({checkedImages});
 
         const { data } = await tesloApi.patch(`/products/${id}`, {
             images: checkedImages,
